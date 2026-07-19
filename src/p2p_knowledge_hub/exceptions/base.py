3c1f21b62f6d9de4bc3e18e14355a90d297e8a7f
Santhosh <sansuganyas@outlook.com>
2026-07-19 22:26:19 +0530

    Implemented hasing but not exception catch

---
+++ src/p2p_knowledge_hub/exceptions/base.py
@@ -0,0 +1,45 @@
+from types import TracebackType
+import traceback
+from pathlib import Path
+from p2p_knowledge_hub.settings.exceptions import ExceptionSettings
+
+
+class P2P_HubException(Exception):
+    settings = ExceptionSettings()
+
+    def __init__(
+        self,
+        message: str | None = None,
+        error_code: str = "P2P_HUB_ERROR",
+        tb: TracebackType | None = None,
+        exc_type: str | None = None,
+    ) -> None:
+        self.message = message or self.settings.default_error_message
+        self.error_code = error_code
+        self.traceback_info = (
+            self._extract(tb, exc_type)
+            if self.settings.include_traceback_in_logs
+            else None
+        )
+        super().__init__(self.message)
+
+    def _extract(
+        self, tb: TracebackType | None = None, exc_type: str | None = None
+    ) -> str | None:
+        if tb is None:
+            return None
+
+        extracted = traceback.extract_tb(tb)
+        if not extracted:
+            return None
+        frame = extracted[-1]
+        return f"{exc_type} at {Path(frame.filename).name} line {frame.lineno}"
+
+    def __str__(self) -> str:
+        if self.traceback_info and self.settings.debug_mode:
+            return f"{self.message} \n {self.traceback_info}"
+        return f"{self.message}"
+
+
+class DocumentException(P2P_HubException):
+    pass
--- src/p2p_knowledge_hub/ingestion/hashing.py
+++ src/p2p_knowledge_hub/ingestion/hashing.py
@@ -1,8 +1,21 @@
 import hashlib
 from pathlib import Path
+from p2p_knowledge_hub.exceptions.base import DocumentException
 
 
 def compute_sha256(file_path: Path) -> str:
-    with open(file_path, "rb") as f:
-        digest = hashlib.file_digest(f, "sha256")
-    return digest.hexdigest()
+    if file_path.is_file():
+        try:
+            with open(file_path, "rb") as f:
+                digest = hashlib.file_digest(f, "sha256")
+            return digest.hexdigest()
+        except PermissionError as e:
+            raise DocumentException(message=f"Unable to read. Access denied. {e}")
+    else:
+        raise DocumentException("Please provide file Path.")
+
+
+if __name__ == "__main__":
+    # ss = compute_sha256(Path("/tmp/hash_permission_test"))
+    ss = compute_sha256(Path("/home/san/annclassification/"))
+    print(ss)
